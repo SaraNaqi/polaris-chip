287f472c6b17cc9df95b98a53cb6bb351d8bc1aa
SaraNaqi <stn5144@psu.edu>
2026-02-08 23:37:24 -0500

    updated polaris chip sn

--- src/my-card.js
+++ src/my-card.js
@@ -19,14 +19,203 @@ export class MyCard extends LitElement {
 
   static get styles() {
     return css`
-      :host {
-        display: block;
-      }
+      .card {
+  max-width:400px;
+  border: 2px solid #ddd;
+  padding: 16px;
+}
+.card-image{
+  height: 200px;
+}
+.details-button{
+  color: red;
+  background-color: black;
+  border-radius: 20%;
+  font-size: 25px;
+  padding:20px;
+   margin: 20px;
+}
+.details-button{
+  display: inline-block;
+}
+.fancy {
+    background-color: blue;
+    border-color: black;
+  }
+@media (max-width: 800px) and (min-width: 501px) {
+  .details-button {
+    display: block;
+  }
+}
+@media (max-width: 500px){
+  .card {
+    max-width: 300px;
+    padding: 12px;
+  }
+  .card-image {
+    height: 150px;
+  }
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div>
+<h1>Card changing controls</h1>
+<h2>Controls</h2>
+<div class="control-wrapper">
+  <button class="duplicate">Clone Card</button>
+  <button class="title">Change title</button>
+  <button class="img-change">Change image</button>
+  <button id="bg-change">Change background</button>
+  <button id="delete">Delete card</button>
+</div>
+
+<div class = "card-list">
+<div class = "card">
+  <div class ="class-header">
+  <img
+       class = "card-image"
+       src = "https://www.w3schools.com/bootstrap4/img_avatar1.png"
+       alt = "card-image"
+       />
+    <h2> Sara Naqi's Card</h2>
+  </div>
+  <div class ="card-body">
+    <div class ="section">
+  <p> this is a card that has a header and details </p>
+    </div>
+    
+    <div>
+    <a class = "details-button" href="https://hax.psu.edu">
+      details
+    </a>
+  </div>
+  </div>
+<h1>Card changing controls</h1>
+<h2>Controls</h2>
+<div class="control-wrapper">
+  <button class="duplicate">Clone Card</button>
+  <button class="title">Change title</button>
+  <button class="img-change">Change image</button>
+  <button id="bg-change">Change background</button>
+  <button id="delete">Delete card</button>
+</div>
+
+<div class = "card-list">
+<div class = "card">
+  <div class ="class-header">
+  <img
+       class = "card-image"
+       src = "https://www.w3schools.com/bootstrap4/img_avatar1.png"
+       alt = "card-image"
+       />
+    <h2> Sara Naqi's Card</h2>
+  </div>
+  <div class ="card-body">
+    <div class ="section">
+  <p> this is a card that has a header and details </p>
+    </div>
+    
+    <div>
+    <a class = "details-button" href="https://hax.psu.edu">
+      details
+    </a>
+  </div>
+  </div>
+<h1>Card changing controls</h1>
+<h2>Controls</h2>
+<div class="control-wrapper">
+  <button class="duplicate">Clone Card</button>
+  <button class="title">Change title</button>
+  <button class="img-change">Change image</button>
+  <button id="bg-change">Change background</button>
+  <button id="delete">Delete card</button>
+</div>
+
+<div class = "card-list">
+<div class = "card">
+  <div class ="class-header">
+  <img
+       class = "card-image"
+       src = "https://www.w3schools.com/bootstrap4/img_avatar1.png"
+       alt = "card-image"
+       />
+    <h2> Sara Naqi's Card</h2>
+  </div>
+  <div class ="card-body">
+    <div class ="section">
+  <p> this is a card that has a header and details </p>
+    </div>
+    
+    <div>
+    <a class = "details-button" href="https://hax.psu.edu">
+      details
+    </a>
+  </div>
+  </div>
+<h1>Card changing controls</h1>
+<h2>Controls</h2>
+<div class="control-wrapper">
+  <button class="duplicate">Clone Card</button>
+  <button class="title">Change title</button>
+  <button class="img-change">Change image</button>
+  <button id="bg-change">Change background</button>
+  <button id="delete">Delete card</button>
+</div>
+
+<div class = "card-list">
+<div class = "card">
+  <div class ="class-header">
+  <img
+       class = "card-image"
+       src = "https://www.w3schools.com/bootstrap4/img_avatar1.png"
+       alt = "card-image"
+       />
+    <h2> Sara Naqi's Card</h2>
+  </div>
+  <div class ="card-body">
+    <div class ="section">
+  <p> this is a card that has a header and details </p>
+    </div>
+    
+    <div>
+    <a class = "details-button" href="https://hax.psu.edu">
+      details
+    </a>
+  </div>
+  </div>
+<h1>Card changing controls</h1>
+<h2>Controls</h2>
+<div class="control-wrapper">
+  <button class="duplicate">Clone Card</button>
+  <button class="title">Change title</button>
+  <button class="img-change">Change image</button>
+  <button id="bg-change">Change background</button>
+  <button id="delete">Delete card</button>
+</div>
+
+<div class = "card-list">
+<div class = "card">
+  <div class ="class-header">
+  <img
+       class = "card-image"
+       src = "https://www.w3schools.com/bootstrap4/img_avatar1.png"
+       alt = "card-image"
+       />
+    <h2> Sara Naqi's Card</h2>
+  </div>
+  <div class ="card-body">
+    <div class ="section">
+  <p> this is a card that has a header and details </p>
+    </div>
+    
+    <div>
+    <a class = "details-button" href="https://hax.psu.edu">
+      details
+    </a>
+  </div>
+  </div></div>`;
   }
 
   static get properties() {
--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -29,6 +29,8 @@ export class PolarisChip extends LitElement {
     super();
     // a variable on this object called title
     this.title = 'Chip Default';
+    this.link = 'psu.edu'
+
   }
 
   // CSS styles are scoped JUST to this element. This uses a technology called
@@ -50,13 +52,13 @@ export class PolarisChip extends LitElement {
       }
 
       span {
-        background-color: orange;
+        background-color: yellow;
         color: black;
         font-size: 24px;
         padding: 16px;
         margin: 8px;
       }
-
+       span :focus-within,
       span:hover {
         background-color: grey;
         border: 1px solid black;
@@ -83,7 +85,7 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<span>${this.title}</span>`;
+    return html`<span><a href="${this.link}">${this.title}</a></span>`;
   }
 
   // LitElement uses the properties call to do the following:
@@ -95,6 +97,7 @@ export class PolarisChip extends LitElement {
     return {
       // this is a String. Array, Object, Number, Boolean are other valid values here
       title: { type: String },
+      link: { type: String },
     };
   }
 }
